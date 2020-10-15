9e673a40246a36268cb120f455d4b10173cdfcd8
ryan982 <39806336+ryan982@users.noreply.github.com>
2020-10-15 08:47:40 +0530

    Update AI.py
    Voice input based youtube automation. Will work with dynamic inputs as well and similar to youtube, any other application can be automated too.

--- AI.py
+++ AI.py
@@ -59,7 +59,7 @@ if open_youtube in query:
         vid_num = int(query)
     print(vid_num)
     vid_path = '/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer['+str(vid_num)+']/div[1]/div/div[1]/div/h3/a/yt-formatted-string'
-    print(vid_path)
+    
 
     play_video = driver.find_element_by_xpath(vid_path)
     play_video.click()
